a67db73fc22d63161c1ceaa090727377f8f9fddb
Aliaksandr_Minchankou <Aliaksandr_Minchankou@epam.com>
2018-11-22 15:43:08 +0300

    Send phone number to server

--- src/components/WeCallYou.js
+++ src/components/WeCallYou.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './WeCallYou.css';
+import axios from 'axios';
 
 class WeCallYou extends Component {
     constructor(props) {
@@ -9,6 +10,7 @@ class WeCallYou extends Component {
         };
 
         this.handleInputPhone = this.handleInputPhone.bind(this);
+        this.handleSendPhone = this.handleSendPhone.bind(this);
     }
 
     handleInputPhone(ev) {
@@ -17,11 +19,27 @@ class WeCallYou extends Component {
         this.setState({ phone });
     }
 
+    handleSendPhone() {
+        console.log('send phone:', this.state.phone);
+        axios.post('/m.php', {
+            phone: '+375' + this.state.phone
+        })
+        .then(function (response) {
+            console.log(response);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <div className="wcu">
                 <div className="wcu-label">We are call you</div>
-                <PhoneInput handleChange={this.handleInputPhone} phone={this.state.phone}/>
+                <PhoneInput handleChange={this.handleInputPhone}
+                            phone={this.state.phone}
+                            handleSend={this.handleSendPhone}
+                />
             </div>
         )
     }
@@ -29,7 +47,19 @@ class WeCallYou extends Component {
 
 function PhoneInput(props) {
     return <div>
-        <input type="text"  pattern="[0-9]{0,9}" onInput={props.handleChange} value={props.phone} />
+        <div className="wcu-phone-wrapper">
+            <input className="wcu-phone-input" type="text"
+                   pattern="[0-9]{0,9}"
+                   onChange={props.handleChange}
+                   value={props.phone}
+                   placeholder="your phone"
+            />
+        </div>
+        <button className="wcu-button-send"
+                onClick={props.handleSend}
+        >
+            send
+        </button>
     </div>;
 }
 
